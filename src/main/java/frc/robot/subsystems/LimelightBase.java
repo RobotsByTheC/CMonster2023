e1660bcc3465139014bd860d7e3513d41bcf5d73
amazonpg <60156246+amazonpg@users.noreply.github.com>
2023-01-18 14:25:58 -0500

    Update LimelightBase.java

--- src/main/java/frc/robot/subsystems/LimelightBase.java
+++ src/main/java/frc/robot/subsystems/LimelightBase.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /*                    TODO
  *        - Basic lineup for the targets (adjust robot to face precisely at retro tape) 
@@ -23,9 +23,12 @@ public class LimelightBase extends SubsystemBase {
 
   NetworkTable limin = NetworkTableInstance.getDefault().getTable("limelight");
     
+
+    //pulls the distance from the identified object in the x direction and y direction and also the object's area
     double tx = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
     double ty = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0);
     double ta = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0);
+
 
   /** Creates a new LimelightBase. */
   public LimelightBase() {}
@@ -33,5 +36,19 @@ public class LimelightBase extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+
+    NetworkTableEntry tx = limin.getEntry("tx");
+    NetworkTableEntry ty = limin.getEntry("ty");
+    NetworkTableEntry ta = limin.getEntry("ta");
+
+    //read values periodically
+    double x = tx.getDouble(0.0);
+    double y = ty.getDouble(0.0);
+    double area = ta.getDouble(0.0);
+
+    //post to smart dashboard periodically
+    SmartDashboard.putNumber("LimelightX", x);
+    SmartDashboard.putNumber("LimelightY", y);
+    SmartDashboard.putNumber("LimelightArea", area);
   }
 }
